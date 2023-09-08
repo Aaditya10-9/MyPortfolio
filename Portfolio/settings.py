90ff66e45a629e76090c68d12bb88fe19d736f4c
Aaditya10-9 <aadityagujjar3@gmail.com>
2023-09-08 23:40:22 +0530

    Initial commit

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7^lqqci#mdet+w&!0ybx2zoxcr!&x-iy^6now@__sx5iy3zxng
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
